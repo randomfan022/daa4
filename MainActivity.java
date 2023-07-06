acff7dae6e24db0bdcd79400d514e7e596879049
randomfan022 <112581442+randomfan022@users.noreply.github.com>
2023-07-06 21:47:30 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,122 +1,56 @@
-package com.example.cam_app_4a_103;
-
-import androidx.annotation.NonNull;
+package com.example.prog4a4bcombined;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import java.io.InputStream;
-
 public class MainActivity extends AppCompatActivity {
-    private final int CAMERA_REQ_CODE = 100;
-    ImageView imgCamera;
-
-    boolean camImgfit;
-
-    Button cameraBtn, selectImgBtn;
-
-    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
-    private static final int REQUEST_CODE_SELECT_IMAGE = 2;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        imgCamera = findViewById(R.id.camImg);
-        cameraBtn = findViewById(R.id.cameraBtn);
-
-        cameraBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(iCamera, CAMERA_REQ_CODE);
-            }
-        });
-
-        selectImgBtn = findViewById(R.id.selectImgBtn);
-
-        selectImgBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(
-                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(
-                            MainActivity.this,
-                            new String[] {
-                                    Manifest.permission.READ_EXTERNAL_STORAGE
-                            } ,
-                            REQUEST_CODE_STORAGE_PERMISSION
-                    );
-                } else {
-                    selectImage();
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == CAMERA_REQ_CODE && resultCode == RESULT_OK) {
-                Bitmap img = (Bitmap) (data.getExtras().get("data"));
-                imgCamera.setImageBitmap(img);
-        }
-
-        if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
-            if(data != null) {
-                Uri selectedImageUri = data.getData();
-                if(selectedImageUri != null) {
-                    try {
-                        InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
-                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                        imgCamera.setImageBitmap(bitmap);
-                    } catch (Exception exception) {
-                        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        }
-
-    }
-
-    private void selectImage() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length > 0) {
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                selectImage();
-            }
-            else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+ Button cam;
+ Button gal;
+ ImageView viewt;
+ @SuppressLint("MissingInflatedId")
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_main);
+ cam=findViewById(R.id.cam);
+ gal=findViewById(R.id.Gallery);
+ viewt=findViewById(R.id.imageView);
+ cam.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View v) {
+ Intent in = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+ startActivityForResult(in,100);
+ }
+ });
+ gal.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View v) {
+ Intent in= new 
+Intent(Intent.ACTION_PICK).setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+ startActivityForResult(in, 101);
+ }
+ });
+ }
+ @Override
+ protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+ super.onActivityResult(requestCode, resultCode, data);
+ if(resultCode==RESULT_OK)
+ {
+ if(requestCode==100)
+ {
+ Bitmap img=(Bitmap)(data.getExtras().get("data"));
+ viewt.setImageBitmap(img);
+ }
+ if(requestCode==101)
+ {
+ viewt.setImageURI(data.getData());
+ }
+ }
+ }
 }
